c46c51c0f6bd7fc8d57c90c1be72cb8cd78496cd
pawn-4-git <pawn4sns+dummy@gmail.com>
2025-07-13 10:22:35 +0900

    dynamodbの設定を作成

---
+++ backend/iac/dynamodb/UsersTable/lib/users_table-stack.ts
@@ -0,0 +1,28 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+
+export class UsersTableStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const table = new dynamodb.Table(this, 'UsersTable', {
+      tableName: 'UsersTable',
+      partitionKey: {
+        name: 'UserId',
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // DEV -> DESTROY, PROD -> RETAIN
+      timeToLiveAttribute: 'ExpiresAt',
+    });
+
+    table.addGlobalSecondaryIndex({
+      indexName: 'AccountNameIndex',
+      partitionKey: {
+        name: 'AccountName',
+        type: dynamodb.AttributeType.STRING,
+      },
+    });
+  }
+}
