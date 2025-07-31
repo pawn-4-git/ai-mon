8c2ed721d213f36ee6c162f7c7b99028b5844565
pawn-4-git <pawn4sns+dummy@gmail.com>
2025-07-31 22:52:28 +0900

    修正

--- backend/src/lambda/scoreHistoryGet/app.mjs
+++ backend/src/lambda/scoreHistoryGet/app.mjs
@@ -1,6 +1,7 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
 import { validateSession } from "/opt/authHelper.js";
+import { updateSession } from "/opt/userHelper.js";
 
 const client = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(client);
@@ -23,12 +24,13 @@ export const lambdaHandler = async (event) => {
         }
 
         const userId = event.pathParameters?.userId;
-        if (!userId) {
+        if (!userId || userId != authResult.userId) {
             return {
                 statusCode: 400,
                 body: JSON.stringify({ message: "User ID is required." }),
             };
         }
+        const sessionResult = await updateSession(authResult.sessionId);
 
         const queryCommand = new QueryCommand({
             TableName: SCORES_TABLE_NAME,
@@ -44,6 +46,14 @@ export const lambdaHandler = async (event) => {
 
         return {
             statusCode: 200,
+            multiValueHeaders: {
+                "Content-Type": ["application/json"], // Content-Typeも配列にする
+                // Set-Cookieを配列として指定する
+                "Set-Cookie": [
+                    `sessionId=${sessionResult.sessionId}; HttpOnly; Secure; SameSite=Strict; Path=/; Max-Age=86400`,
+                    `sessionVersionId=${sessionResult.sessionVersionId}; HttpOnly; Secure; SameSite=Strict; Path=/; Max-Age=86400`
+                ]
+            },
             body: JSON.stringify({
                 message: "Score history retrieved successfully.",
                 scores: response.Items || [],
