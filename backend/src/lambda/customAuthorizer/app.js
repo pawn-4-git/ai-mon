718c87d382aca011ba5d7f2fd967f9028056a5bc
pawn-4-git <pawn4sns+dummy@gmail.com>
2025-08-13 08:09:40 +0900

    ペイロードフォーマット2.0対応

--- backend/src/lambda/customAuthorizer/app.js
+++ backend/src/lambda/customAuthorizer/app.js
@@ -1,33 +1,21 @@
 // backend/src/lambda/customAuthorizer/app.js
+const crypto = require('crypto');
+
 exports.lambdaHandler = async (event, context) => {
   const secretHeader = process.env.CLOUDFRONT_SECRET_HEADER;
   const incomingSecret = event.headers['x-cloudfront-secret'];
 
-  let effect = 'Deny';
-  if (
+  const isAuthorized =
     secretHeader &&
     incomingSecret &&
     Buffer.from(secretHeader).length === Buffer.from(incomingSecret).length &&
-    require('crypto').timingSafeEqual(Buffer.from(secretHeader), Buffer.from(incomingSecret))
-  ) {
-    effect = 'Allow';
-  } else {
+    crypto.timingSafeEqual(Buffer.from(secretHeader), Buffer.from(incomingSecret));
+
+  if (!isAuthorized) {
     console.log('Forbidden: Missing or incorrect secret header.');
   }
 
-  const policy = {
-    principalId: 'user', // The principal user identification associated with the token sent by the client.
-    policyDocument: {
-      Version: '2012-10-17',
-      Statement: [
-        {
-          Action: 'execute-api:Invoke',
-          Effect: effect,
-          Resource: event.methodArn,
-        },
-      ],
-    },
+  return {
+    isAuthorized: isAuthorized,
   };
-
-  return policy;
 };
