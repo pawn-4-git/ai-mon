0564feee73ff325d08a091bcd5d0b4151c950cec
pawn-4-git <pawn4developer+github@gmail.com>
2025-08-13 08:41:56 +0900

    Revert "ペイロードフォーマット2.0対応"

--- backend/src/lambda/customAuthorizer/app.js
+++ backend/src/lambda/customAuthorizer/app.js
@@ -1,19 +1,33 @@
 // backend/src/lambda/customAuthorizer/app.js
-const crypto = require('crypto');
-
 exports.lambdaHandler = async (event, context) => {
   const secretHeader = process.env.CLOUDFRONT_SECRET_HEADER;
   const incomingSecret = event.headers['x-cloudfront-secret'];
 
-  const isAuthorized =
+  let effect = 'Deny';
+  if (
     secretHeader &&
     incomingSecret &&
     Buffer.from(secretHeader).length === Buffer.from(incomingSecret).length &&
-    crypto.timingSafeEqual(Buffer.from(secretHeader), Buffer.from(incomingSecret));
-
-  if (!isAuthorized) {
+    require('crypto').timingSafeEqual(Buffer.from(secretHeader), Buffer.from(incomingSecret))
+  ) {
+    effect = 'Allow';
+  } else {
     console.log('Forbidden: Missing or incorrect secret header.');
   }
 
-  return { isAuthorized };
+  const policy = {
+    principalId: 'user', // The principal user identification associated with the token sent by the client.
+    policyDocument: {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Action: 'execute-api:Invoke',
+          Effect: effect,
+          Resource: event.methodArn,
+        },
+      ],
+    },
+  };
+
+  return policy;
 };
